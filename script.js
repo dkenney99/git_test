d272a7c4cd44ba171eb5ea61422333a627892148
Daniel Kenney <danielkenney99@yahoo.com>
2022-11-07 22:16:07 -0800

    messing around with objects

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-const btn = document.querySelector("#btn");
-btn.addEventListener("click", () => {
-  alert("Hello World");
-});
+// function Book(title, author, pages, read) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.read = read;
+//   this.info = function () {
+//     if (read) {
+//       return `${title} by ${author}, ${pages}, has been read`;
+//     } else {
+//       return `${title} by ${author}, ${pages}, has not been read`;
+//     }
+//   };
+// }
+
+const userOne = {
+  email: "dkenney@sdsu.edu",
+  name: "Daniel",
+  login: function () {
+    console.log(this.email, "has logged in");
+  },
+  logout: function () {
+    console.log(this.email, "has logged out");
+  },
+};
+
+console.log(userOne.name);
