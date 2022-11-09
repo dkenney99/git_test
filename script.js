57d24a305d1b2545378f899acfa739c0937cf472
Daniel Kenney <danielkenney99@yahoo.com>
2022-11-08 18:25:58 -0800

    learning oop and constructors/classes

--- script.js
+++ script.js
@@ -12,15 +12,45 @@
 //   };
 // }
 
-const userOne = {
-  email: "dkenney@sdsu.edu",
-  name: "Daniel",
-  login: function () {
-    console.log(this.email, "has logged in");
-  },
-  logout: function () {
-    console.log(this.email, "has logged out");
-  },
-};
+class User {
+  constructor(email, name) {
+    this.email = email;
+    this.name = name;
+    this.score = 0;
+  }
+  login() {
+    console.log(this.email, "just logged in");
+    return this;
+  }
+  logout() {
+    console.log(this.email, "just logged out");
+    return this;
+  }
+  updateScore() {
+    this.score++;
+    console.log(this.email, "score is now", this.score);
+    return this;
+  }
+}
 
-console.log(userOne.name);
+class Admin extends User {
+  deleteUser(user) {
+    users = users.filter((u) => {
+      return u.email != user.email;
+    });
+  }
+}
+
+var userOne = new User("dkenney@sdsu.edu", "Daniel");
+var userTwo = new User("yoshi@hotmail.com", "Yoshi");
+var admin = new Admin("admin@yahoo.com", "Kenney");
+
+var users = [userOne, userTwo, admin];
+
+// admin.deleteUser(userOne);
+userOne.deleteUser(userTwo);
+
+// the "new" keyword
+// - creates a new empty object
+// - sets the value of "this" to be the new empty object
+// - calls the constructor method
