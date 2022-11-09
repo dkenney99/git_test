2edf83a00b4a6b3b006bb2b663dcf002b6cc7c91
Daniel Kenney <danielkenney99@yahoo.com>
2022-11-08 23:48:28 -0800

    async requests

--- script.js
+++ script.js
@@ -1,56 +1,11 @@
-// function Book(title, author, pages, read) {
-//   this.title = title;
-//   this.author = author;
-//   this.pages = pages;
-//   this.read = read;
-//   this.info = function () {
-//     if (read) {
-//       return `${title} by ${author}, ${pages}, has been read`;
-//     } else {
-//       return `${title} by ${author}, ${pages}, has not been read`;
-//     }
-//   };
-// }
+const request = new XMLHttpRequest();
 
-class User {
-  constructor(email, name) {
-    this.email = email;
-    this.name = name;
-    this.score = 0;
+request.addEventListener("readystatechange", () => {
+  // console.log(request, request.readyState);
+  if (request.readyState === 4) {
+    console.log(request.responseText);
   }
-  login() {
-    console.log(this.email, "just logged in");
-    return this;
-  }
-  logout() {
-    console.log(this.email, "just logged out");
-    return this;
-  }
-  updateScore() {
-    this.score++;
-    console.log(this.email, "score is now", this.score);
-    return this;
-  }
-}
+});
 
-class Admin extends User {
-  deleteUser(user) {
-    users = users.filter((u) => {
-      return u.email != user.email;
-    });
-  }
-}
-
-var userOne = new User("dkenney@sdsu.edu", "Daniel");
-var userTwo = new User("yoshi@hotmail.com", "Yoshi");
-var admin = new Admin("admin@yahoo.com", "Kenney");
-
-var users = [userOne, userTwo, admin];
-
-// admin.deleteUser(userOne);
-userOne.deleteUser(userTwo);
-
-// the "new" keyword
-// - creates a new empty object
-// - sets the value of "this" to be the new empty object
-// - calls the constructor method
+request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+request.send();
