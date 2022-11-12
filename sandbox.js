f7d9cd1be837867e57cba2162edcd0a2b7aaa203
Daniel Kenney <danielkenney99@yahoo.com>
2022-11-12 12:53:03 -0800

    form validation practice

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,31 @@
-const button = document.querySelector("button");
-const popup = document.querySelector(".popup-wrapper");
-const close = document.querySelector(".popup-close");
+const form = document.querySelector(".signup-form");
+const feedback = document.querySelector(".feedback");
+const usernamePattern = /^[a-zA-Z0-9]{6,12}$/;
 
-button.addEventListener("click", () => {
-  popup.style.display = "block";
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //validation
+
+  const username = form.username.value;
+  console.log(usernamePattern.test(username));
+
+  if (usernamePattern.test(username)) {
+    feedback.textContent = "that username is valid";
+  } else {
+    feedback.textContent =
+      "username must be letter only between 6-12 characters long";
+  }
 });
 
-close.addEventListener("click", () => {
-  popup.style.display = "none";
-});
+// live feedback
 
-popup.addEventListener("click", () => {
-  popup.style.display = "none";
+form.username.addEventListener("keyup", (e) => {
+  console.log(e);
+  //console.log(e.target.value, form.username.value);
+  if (usernamePattern.test(e.target.value)) {
+    form.username.setAttribute("class", "success");
+  } else {
+    form.username.setAttribute("class", "error");
+  }
 });
