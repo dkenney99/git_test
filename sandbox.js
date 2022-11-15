09c15230d08af36ba91a3a83907060c4f4102824
Daniel Kenney <danielkenney99@yahoo.com>
2022-11-14 16:18:47 -0800

    catching errors with throw new Error

--- sandbox.js
+++ sandbox.js
@@ -1,28 +1,20 @@
 // async and await
 
 const getTodos = async () => {
-  const response = await fetch("todos.json");
+  const response = await fetch("todoss.json");
+
+  if (response.status !== 200) {
+    throw new Error("cannot fetch the data");
+  }
+
   const data = await response.json();
   return data;
 };
-console.log(1);
-console.log(2);
 
-getTodos().then((data) => {
-  console.log("resolved:", data);
-});
-
-console.log(3);
-console.log(4);
-
-// fetch("todos.json")
-//   .then((response) => {
-//     console.log("resolved", response);
-//     return response.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//   })
-//   .catch((err) => {
-//     console.log("rejected", err);
-//   });
+getTodos()
+  .then((data) => {
+    console.log("resolved:", data);
+  })
+  .catch((err) => {
+    console.log("rejected", err.message);
+  });
