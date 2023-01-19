81cddf50049fe6ad902f0278c138509d5ff36d4c
Daniel Kenney <danielkenney99@yahoo.com>
2023-01-19 12:05:52 -0800

    prototype and constructors

--- sandbox.js
+++ sandbox.js
@@ -1,20 +1,49 @@
-// async and await
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    if (read) {
+      return `${title} by ${author}, ${pages} pages, has been read`;
+    } else {
+      return `${title} by ${author}, ${pages} pages, has not been read`;
+    }
+  };
+}
 
-const getTodos = async () => {
-  const response = await fetch("todoss.json");
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkein", 304, true);
 
-  if (response.status !== 200) {
-    throw new Error("cannot fetch the data");
-  }
+console.log(theHobbit.info());
 
-  const data = await response.json();
-  return data;
+function Plant() {
+  this.country = "Mexico";
+  this.isOrganic = true;
+}
+
+// Add the showNameAndColor method to the Plant prototype property
+Plant.prototype.showNameAndColor = function () {
+  console.log("I am a " + this.name + " and my color is " + this.color);
 };
 
-getTodos()
-  .then((data) => {
-    console.log("resolved:", data);
-  })
-  .catch((err) => {
-    console.log("rejected", err.message);
-  });
+// Add the amIOrganic method to the Plant prototype property
+Plant.prototype.amIOrganic = function () {
+  if (this.isOrganic) console.log("I am organic, Baby!");
+};
+
+function Fruit(fruitName, fruitColor) {
+  this.name = fruitName;
+  this.color = fruitColor;
+}
+
+// Set the Fruit's prototype to Plant's constructor, thus inheriting all of Plant.prototype methods and properties.
+Fruit.prototype = new Plant();
+
+// Creates a new object, aBanana, with the Fruit constructor
+var aBanana = new Fruit("Banana", "Yellow");
+
+// Here, aBanana uses the name property from the aBanana object prototype, which is Fruit.prototype:
+console.log(aBanana.name); // Banana
+
+// Uses the showNameAndColor method from the Fruit object prototype, which is Plant.prototype. The aBanana object inherits all the properties and methods from both the Plant and Fruit functions.
+console.log(aBanana.showNameAndColor()); // I am a Banana and my color is yellow.
